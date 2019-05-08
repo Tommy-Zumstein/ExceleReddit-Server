73578ef4e65e40d6d67aebbed788a72889001306
Scott <littles1@seattleu.edu>
2019-05-07 19:28:37 -0700

    Added user endpoint and mongoose schema models

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 "use strict";
 exports.__esModule = true;
 var express = require("express");
-var logger = require("morgan");
-var url = require("url");
 var bodyParser = require("body-parser");
+var UserRouter = require('./routes/users');
 // Creates and configures an ExpressJS web server.
 var App = /** @class */ (function () {
     //Run configuration methods on the Express instance.
@@ -14,7 +13,6 @@ var App = /** @class */ (function () {
     }
     // Configure Express middleware.
     App.prototype.middleware = function () {
-        this.express.use(logger('dev'));
         this.express.use(bodyParser.json());
         this.express.use(bodyParser.urlencoded({ extended: false }));
     };
@@ -24,48 +22,8 @@ var App = /** @class */ (function () {
          * working so far. This function will change when we start to add more
          * API endpoints */
         var router = express.Router();
-        // router.get('/', (req, res, next) => {
-        //   res.json({
-        //     message: 'Hello World!'
-        //   });
-        // });
-        router.get('/one', function (req, res, next) {
-            res.send('request one');
-        });
-        router.get('/add', function (req, res, next) {
-            var urlParts = url.parse(req.url, true);
-            var query = urlParts.query;
-            console.log('var1:' + query.var1);
-            console.log('var2:' + query.var2);
-            var value1 = parseInt(query.var1);
-            var value2 = parseInt(query.var2);
-            var sum = value1 + value2;
-            //var sum = query.var1 + query.var2;
-            var msg = 'addition of ' + query.var1 + ' plus ' + query.var2 + ' equals ' + sum;
-            console.log(msg);
-            res.send(msg);
-        });
-        var fname2;
-        router.get('/name/:fname', function (req, res, next) {
-            var name;
-            console.log(':fname = ' + req.params.fname);
-            if (req.params.fname === 'israelh') {
-                name = fname2 + ' hilerio';
-            }
-            else {
-                name = fname2 + ' world';
-            }
-            console.log(name);
-            res.send("Your name is: " + name);
-        });
-        router.param('fname', function (req, res, next, value) {
-            console.log('The param value is: ' + value);
-            fname2 = value + "-ABC";
-            next();
-        });
-        this.express.use('/', router);
-        this.express.use('/images', express.static(__dirname + '/img'));
-        this.express.use('/', express.static(__dirname + '/pages'));
+        router.get('/', UserRouter);
+        this.express.use(UserRouter);
     };
     return App;
 }());
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  res.send(User);
 });
 
 module.exports = router;
